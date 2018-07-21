9d235188a91d9fba3719c3b630de12d2463f2b20
shiv-sundar <ssundar@iastate.edu>
2018-07-21 16:09:07 -0500

    add image chooser

--- app/src/main/java/com/test/hackmw/getImage.java
+++ app/src/main/java/com/test/hackmw/getImage.java
@@ -1,13 +1,33 @@
 package com.test.hackmw;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.net.Uri;
 
 public class getImage extends AppCompatActivity {
+    private static final int CHOOSE_IMAGE = 101;
+    Uri uriProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_image);
+        chooseProfilePicture();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uriProfile = data.getData();
+        }
+    }
+
+    private void chooseProfilePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "select your profile picture"), CHOOSE_IMAGE);
     }
 }
